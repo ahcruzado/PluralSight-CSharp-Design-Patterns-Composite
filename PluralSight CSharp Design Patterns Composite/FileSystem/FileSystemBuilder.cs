89c5fa2304ae25ad124e2962a23b3b99a9d7b7e6
ahcruzado <ahcruzado@users.noreply.github.com>
2020-03-29 16:54:21 -0300

    SetCurrentDirectory

---
+++ PluralSight CSharp Design Patterns Composite/FileSystem/FileSystemBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluralSight_CSharp_Design_Patterns_Composite.FileSystem
+{
+    public class FileSystemBuilder
+    {
+        private DirectoryItem currentDirectory;
+
+        public FileSystemBuilder(string rootDiectoryName)
+        {
+            this.Root = new DirectoryItem(rootDiectoryName);
+            this.currentDirectory = this.Root;
+        }
+
+        internal DirectoryItem AddDirectory(string name)
+        {
+            var dir= new DirectoryItem(name);
+            this.currentDirectory.Add(dir);
+            this.currentDirectory = dir;
+            return dir;
+        }
+
+        internal DirectoryItem SetCurrentDirectory(string directoryName)
+        {
+            var dirStack = new Stack<DirectoryItem>();
+            dirStack.Push(this.Root);
+
+            while (dirStack.Any())
+            {
+                var current = dirStack.Pop();
+                if (current.Name==directoryName)
+                {
+                    this.currentDirectory = current;
+                    return current;
+                }
+                foreach (var item in current.Items.OfType<DirectoryItem>())
+                {
+                    dirStack.Push(item);
+                }
+            }
+            throw new InvalidOperationException($"Directory name '{directoryName}' not found!");
+        }
+
+        internal FileItem AddFile(string name, long fileBytes)
+        {
+            var file = new FileItem(name, fileBytes);
+            this.currentDirectory.Add(file);
+            return file;
+        }
+
+        internal DirectoryItem Root { get; }
+
+    }
+}
--- PluralSight CSharp Design Patterns Composite/FileSystem/FileSystemExample.cs
+++ PluralSight CSharp Design Patterns Composite/FileSystem/FileSystemExample.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,26 +11,21 @@ namespace PluralSight_CSharp_Design_Patterns_Composite.FileSystem
     {
         public static void Run()
         {
-            var root = new DirectoryItem("Development");
-            var proj1 = new DirectoryItem("Porject1");
-            var proj2 = new DirectoryItem("Porject2");
-            root.Add(proj1);
-            root.Add(proj2);
+            var builder = new FileSystemBuilder("development");
+            builder.AddDirectory("project1");
+            builder.AddFile("p1f3.txt", 4100);
+            builder.AddFile("p1f2.txt", 3100);
+            builder.AddDirectory("sub-dir1");
+            builder.AddFile("p1f3.txt", 4100);
+            builder.AddFile("p1f4.txt", 5100);
 
-            proj1.Add(new FileItem("p1f1.txt", 2100));
-            proj1.Add(new FileItem("p1f2.txt", 3100));
+            builder.SetCurrentDirectory("development");
+            builder.AddDirectory("projec2");
+            builder.AddFile("p2f1.text", 6100);
+            builder.AddFile("p2f2.text", 7100);
 
-            var subDir1 = new DirectoryItem("sub-Dir1");
-            subDir1.Add(new FileItem("p1f3.txt", 4100));
-            subDir1.Add(new FileItem("p1f4.txt", 5100));
-            proj1.Add(subDir1);
-
-            proj2.Add(new FileItem("p2f1.txt", 6100));
-            proj2.Add(new FileItem("p2f2.txt", 7100));
-
-            Console.WriteLine($"Total size (proj2): {proj2.GetSizeInKb()}");
-            Console.WriteLine($"Total size (proj1): {proj1.GetSizeInKb()}");
-            Console.WriteLine($"Total size (root): {root.GetSizeInKb()}");
+            Console.WriteLine($"Total size (root) : {builder.Root.GetSizeInKb()}");
+            Console.WriteLine(JsonConvert.SerializeObject(builder.Root, Formatting.Indented));
         }
     }
 }
